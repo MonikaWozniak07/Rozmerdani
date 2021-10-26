f45b9b3ea3ef509aa9d0d38f5dabeffa1e8516da
Monika <monikawozniak07@gmail.com>
2021-10-26 23:19:21 +0200

    feat hamburger

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 /* test */
+
+const hamburgerBody = document.querySelector('.hamburger-list');
+const hamburgerIcon = document.querySelector('.nav-hamburger');
+
+hamburgerIcon.addEventListener('click',function(){
+    hamburgerBody.style.left = 50 + 'vw';
+})
