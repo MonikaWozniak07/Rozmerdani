8a9a98a57397b76939d5a688f8ef54b6d9815dbc
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2022-01-22 22:21:02 +0100

    usuniecie zakomentowanego kodu

--- js/app.js
+++ js/app.js
@@ -1,23 +1,3 @@
-/* test */
-
-// const hamburgerBody = document.querySelector(".hamburger-list");
-// const hamburgerIcon = document.querySelector(".fa-bars");
-// const xIcon = document.querySelector(".fa-times");
-
-// xIcon.classList.add("none");
-
-// hamburgerIcon.addEventListener("click", function () {
-//     hamburgerBody.style.right = 0 + "vw";
-//     hamburgerIcon.classList.add("none");
-//     xIcon.classList.remove("none");
-// });
-
-// xIcon.addEventListener("click", function () {
-//     hamburgerBody.style.right = -50 + "vw";
-//     hamburgerIcon.classList.remove("none");
-//     xIcon.classList.add("none");
-// });
-
 const menuToggler = document.querySelector(".menu-toggler");
 const mainMenu = document.querySelector(".main");
 
