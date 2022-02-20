9a3f0c214a9f029896d2b7f6b654a9d425f7090a
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2022-02-21 00:21:57 +0100

    remove unused old php

--- backup/content-article.php
+++
@@ -1,8 +0,0 @@
-<div style="margin: 20px 0;">
-    <div><?php the_date(); ?></div>
-    <div>
-        <h1><?php the_title(); ?></h1>
-    </div>
-    <div><?php the_tags(); ?></div>
-    <div><?php the_content(); ?></div>
-</div>
