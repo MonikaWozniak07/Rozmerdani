5280498e2b6b7a5049b98dc4c4e1e13af51520b9
Monika <monikawozniak07@gmail.com>
2023-03-24 18:58:05 +0100

    galeria

--- galeria.js
+++ galeria.js
@@ -8,10 +8,9 @@ fetch(fetchRozmerdani).then(function(response){
     let nums=[]
     for(let i=0;i<limit;i++){
         nums.push(i)
-        document.querySelector(`.rozmerdany-container:nth-of-type(${nums[i]+1})`).innerHTML = `<div class="person_img rozmerdany_img"><img src=${res.rozmerdani[nums[i]].img}>
-    </div><div class="person_text rozmerdany_text">
+        document.querySelector(`.rozmerdany-container:nth-of-type(${nums[i]+1})`).innerHTML = `<div class="person_img rozmerdany_img"><img src=${res.rozmerdani[nums[i]].img}></div><div class="person_text rozmerdany_text">
         <h2>${res.rozmerdani[nums[i]].name}</h2>
-       <p>${res.rozmerdani[nums[i]].opis}</p>
+        <p>${res.rozmerdani[nums[i]].opis}</p>
         </div>` 
     }
 
