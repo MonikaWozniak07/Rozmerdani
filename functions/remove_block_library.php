d302d7b870a1f75ae2acc3bc85b5f51a683b39db
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2023-05-17 20:50:45 +0200

    html -> 404

--- functions/remove_block_library.php
+++
@@ -1,7 +0,0 @@
-<?php
-function remove_wp_block_library_css(){
-    wp_dequeue_style( 'wp-block-library' );
-    wp_dequeue_style( 'wp-block-library-theme' );
-    wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
-}
-add_action( 'wp_enqueue_scripts', 'remove_wp_block_library_css', 100 );
--- functions/title_tag.php
+++
@@ -1,7 +0,0 @@
-<?php
-// title tag
-function theme_setup() {
-	// Adds <title> tag support
-	add_theme_support( 'title-tag' );
-}
-add_action( 'after_setup_theme', 'theme_setup');
